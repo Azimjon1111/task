1147fad13524ad58acd139d07254f5ced4e60399
Azimjon1111 <azimjon.nodir@gmail.com>
2022-12-05 15:49:45 +0500

    react redux toolkit added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,18 @@
-import '../styles/globals.css'
+import * as React from 'react';
+import { Provider } from 'react-redux'
+import { createWrapper } from 'next-redux-wrapper'
 import type { AppProps } from 'next/app'
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import store  from '../store'
+import '../styles/globals.css'
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+          <Provider store={store}>
+            <Component {...pageProps}>
+            </Component>
+          </Provider>
+  )
 }
+
+const makeStore = ()=>store;
+const wrapper = createWrapper(makeStore)
+export default wrapper.withRedux(MyApp)
